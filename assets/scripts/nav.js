6900aad0b81c43fbf9630e8d487616418141d765
Qiao Lin <qiaoclin@gmail.com>
2018-01-08 22:36:39 +0100

    index page updated

--- assets/scripts/nav.js
+++ assets/scripts/nav.js
@@ -1,7 +1,17 @@
+var projectKey = location.pathname.split('/')[2]
+
 function openNav() {
     $('nav').css('width', '250px');
+    $(`[data-key=${projectKey}]`).eq(0).addClass('highlight');
+
 }
 
 function closeNav() {
     $('nav').css('width', '0px');
 }
+
+$(document).ready(function(){
+
+  console.log("projectKey ::::",projectKey);
+
+});
