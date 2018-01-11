66863a27316a06b2f1f645024ade59fabf7a9843
Qiao Lin <qiaoclin@gmail.com>
2018-01-11 16:38:09 +0100

    excerpts added

--- assets/scripts/noble_self.js
+++ assets/scripts/noble_self.js
@@ -4,3 +4,24 @@ $(document).ready(function(){
     $(this).parent().siblings().find('.circle-container').toggle();
   });
 });
+
+
+
+  function openExcerpt(evt, indexNum) {
+    var i, tabcontent, tablinks;
+    tabcontent = $(".tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = $(".tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(indexNum).style.display = "block";
+    $('.active').toggleClass('active');
+    evt.currentTarget.className += " active";
+
+}
+
+
+$("#defaultOpen").click();
